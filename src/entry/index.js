07e43d20309c227607a620eafa0d633b08cf9bb2
yeliex <yeliex@yeliex.com>
2016-06-24 15:10:13 +0800

    add sky

--- src/entry/index.js
+++ src/entry/index.js
@@ -5,12 +5,14 @@
  */
 
 import electron, { app, BrowserWindow } from 'electron';
+import {join} from 'path';
 
 let window;
 
 const createWindow = function createWindow() {
   window = new BrowserWindow({ width: 800, height: 600 });
-  window.loadURL('http://192.241.128.187/current/examples/fps.html');
+  window.loadURL(`file://${join(__dirname,'../view/sky.html')}`);
+  // window.loadURL('https://raw.githubusercontent.com/WhitestormJS/whitestorm.js/b22c8036f168ad7cb3e10bc5f4f52bab0651f655/examples/skybox.html');
 };
 
 app.on('ready', createWindow);
